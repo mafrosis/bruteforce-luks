819b6b1b5830701dd44730dfef30dfcd1c753c98
Matt Black <github@mafro.net>
2018-11-10 14:23:59 +1100

    Specify character set

--- doit.sh
+++ doit.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-time bruteforce-luks -t 4 -l 5 -m 5 -w ./state.txt ./luks-header
+time bruteforce-luks -t 4 -l 5 -m 5 -s "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#;!$%_" -w ./state.txt ./luks-header
