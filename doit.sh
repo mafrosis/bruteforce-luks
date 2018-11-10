da311dad143cb95fed40840d1823e0c4832c5dae
Matt Black <github@mafro.net>
2018-11-10 14:24:36 +1100

    Pass num threads as parameter

--- doit.sh
+++ doit.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-time bruteforce-luks -t 4 -l 5 -m 11 -s "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#;!$%_" -w ./state.txt ./luks-header
+THREADS=${1:-4}
+time bruteforce-luks -t "${THREADS}" -l 5 -m 11 -s "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#;!$%_" -w ./state.txt ./luks-header
